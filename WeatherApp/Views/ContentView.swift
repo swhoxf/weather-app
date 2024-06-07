1a6688c30432dfe2ec0c9eec5ed7f7feb76424ff
swhoxf <108375735+swhoxf@users.noreply.github.com>
2024-06-07 18:15:40 -0400

    added LocationManager, ContentView, LoadingView, WelcomeView

---
+++ WeatherApp/Views/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Soph Ho on 6/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    var body: some View {
+        VStack {
+            if let location = locationManager.location {
+                Text("Your coordinates are: \(location.longitude), \(location.latitude)")
+            } else {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+        }
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  WeatherApp
+//
+//  Created by Soph Ho on 6/1/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
